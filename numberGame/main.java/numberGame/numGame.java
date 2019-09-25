ce8a2d2b13f79519b068b13d9efa470e32386222
elca337 <leemcasey14@gmail.com>
2019-09-25 21:21:55 +0100

    Add files via upload

---
+++ numberGame/main.java/numberGame/numGame.java
@@ -0,0 +1,44 @@
+package numberGame;
+
+
+
+import java.util.Scanner;
+
+public class numGame {
+	
+	public static void main(String[] args) {
+		int randomNumber = (int) (Math.random() * 100) + 1;
+		boolean hasWon = false;
+		
+		System.out.println("I have randomly chosen a number between 1 and 100");
+		System.out.println("Try to guess it...");
+		
+		Scanner scanner = new Scanner(System.in);
+		for (int i = 10; 1 > 0; i--) {
+			System.out.println("You have " + i + " guesses left, choose again");
+			int guess = scanner.nextInt();
+			
+			if (randomNumber < guess) {
+				System.out.println("It's smaller than " + guess + ".");
+			}
+			if (randomNumber > guess) {
+				System.out.println("It's higher than " + guess + ".");
+			}
+			
+			if (randomNumber == guess) {
+				hasWon = true;
+				break;
+			}					
+		}
+		
+		if (hasWon) {
+			System.out.println("CORRECT!!!      YOU WIN!!!");
+		} else {
+			System.out.println("GAME OVER      YOU LOSE");
+			System.out.println("The number was :" + randomNumber);
+		}
+	}
+
+}
+
+
